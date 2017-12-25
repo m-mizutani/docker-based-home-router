8bffde03e3c2ce1f1053c8df476a6bbc38f53599
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2017-12-25 15:17:31 +0000

    Add mysql initialize script

---
+++ kea/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat <<EOF > $HOME/.my.cnf
+[client]
+password="$MYSQL_ROOT_PASSWORD"
+EOF
+
+sleep 3
+if [ -z "`mysqlshow -u root -h 127.0.0.1 | grep dhcpdb`" ] ; then
+		mysql -u root -h 127.0.0.1 -e "CREATE DATABASE dhcpdb;"
+		mysql -u root -h 127.0.0.1 -e "SOURCE /usr/share/kea-admin/scripts/mysql/dhcpdb_create.mysql;" dhcpdb
+fi
+
+exec kea-dhcp4 -c /etc/kea/kea-config.json
