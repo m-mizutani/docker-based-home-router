b1fc4434c599a2e98fa17d4664a1c5e985d290c4
Masayoshi Mizutani <mizutani@sfc.wide.ad.jp>
2017-12-25 15:23:39 +0000

    Fix startup script for kea to change mysql password

--- kea/startup.sh
+++ kea/startup.sh
@@ -11,4 +11,5 @@ if [ -z "`mysqlshow -u root -h 127.0.0.1 | grep dhcpdb`" ] ; then
 		mysql -u root -h 127.0.0.1 -e "SOURCE /usr/share/kea-admin/scripts/mysql/dhcpdb_create.mysql;" dhcpdb
 fi
 
+sed -i -e "s@__MYSQL_ROOT_PASSWORD__@$MYSQL_ROOT_PASSWORD@g" /etc/kea/kea-config.json
 exec kea-dhcp4 -c /etc/kea/kea-config.json
